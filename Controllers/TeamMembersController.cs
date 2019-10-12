4f6c8da8dc9bd08528638457cb9071383c7b2176
Jonathan <jester1570@gmail.com>
2019-10-11 20:58:38 -0700

    Using Html.BeginForms for employee search and filtering. Added reset button.

--- Controllers/TeamMembersController.cs
+++ Controllers/TeamMembersController.cs
@@ -53,10 +53,10 @@ namespace TMM_Asp.Controllers
         }
 
         // GET: TeamMembers/FilterTeam
-        public IActionResult FilterTeam()
+        public IActionResult FilterTeam(List<string> filters)
         {
             var employees = BuildTestRecords();
-            var team = MakeTeam(employees);
+            var team = GetFilteredList(employees, filters);
 
             return View(team);
 
@@ -64,25 +64,76 @@ namespace TMM_Asp.Controllers
 
 
         // GET: TeamMembers/Search
-        public IActionResult Search()
+        public IActionResult Search(List<string> names)
         {
             var employees = BuildTestRecords();
-            var team = MakeTeam(employees);
+            var team = BuildSearchList(employees, names);
+
 
             return View(team);
         }
 
+        public IActionResult Delete(List<string> employee)
+        {
+            // TODO: SQL Remove from database
+            // foreach (var id in employee)
+            //    Remove from database user with id
+            var employees = BuildTestRecords();
+            var team = MakeTeam(employees);
+            return View(team);
+        }
 
+
+        // Build Search List
+        private TeamMembersModel BuildSearchList(List<MemberModel> employees, List<string> names)
+        {
+            var members = new List<MemberModel>();
+            foreach (var employee in employees)
+            {
+                foreach (var name in names)
+                {
+                    if (employee.FirstName == name)
+                        members.Add(employee);
+                    else if (employee.LastName == name)
+                        members.Add(employee);
+                }
+            }
+
+            return MakeTeam(members);
+        }
+        // Get Filtered List
+        private TeamMembersModel GetFilteredList(List<MemberModel> employees, List<string> filters)
+        {
+            var members = new List<MemberModel>();
+
+            foreach (var employee in employees)
+            {
+                foreach (var filter in filters)
+                {    if (employee.Department == filter)
+                        members.Add(employee);
+                    else if (employee.EmploymentStatus == filter)
+                        members.Add(employee);
+                    else if (employee.Position == filter)
+                        members.Add(employee);
+                    else if (employee.Shift == filter)
+                        members.Add(employee);
+                    else if (employee.Manager == filter)
+                        members.Add(employee);
+                }
+            }
+
+            return MakeTeam(members);
+        }
         // Build test records.
         List<MemberModel> BuildTestRecords ()
         {
             var employees = new List<MemberModel>
             {
                 new MemberModel{
-                    FirstName = "Dave",
-                    LastName = "Manoa",
-                    Address = "554 E 1150 N",
-                    EmailAddress = "dave.manoa@gmail.com",
+                    FirstName = "Jack",
+                    LastName = "Black",
+                    Address = "1612 Guitar Road",
+                    EmailAddress = "jack.black@gmail.com",
                     PhoneNumber = "801-222-3333",
                     Position = "Bossman",
                     Department = "The World",
@@ -90,7 +141,7 @@ namespace TMM_Asp.Controllers
                     EndDate = "N/A",
                     EmploymentStatus = "active",
                     Shift = "24/7",
-                    Manager = "God",
+                    Manager = "Himself",
                     Photo = "None",
                     FavoriteColor = "Green"
                 },
@@ -98,33 +149,33 @@ namespace TMM_Asp.Controllers
             new MemberModel {
                 FirstName = "Jonathan",
                 LastName = "Manoa",
-                Address = "738 Patterson Road",
+                Address = "123 Unknown Drive",
                 EmailAddress = "j.manoa1570@gmail.com",
                 PhoneNumber = "805-345-5168",
                 Position = "Bossman",
                 Department = "The World",
-                StartDate = "22 May 1987",
+                StartDate = "1 Jan 1900",
                 EndDate = "N/A",
                 EmploymentStatus = "active",
                 Shift = "24/7",
-                Manager = "God",
+                Manager = "Me",
                 Photo = "None",
                 FavoriteColor = "Blue"
             },
 
             new MemberModel {
-                FirstName = "Adin",
-                LastName = "Manoa",
-                Address = "554 E 1150 N",
-                EmailAddress = "adin.manoa@gmail.com",
+                FirstName = "The",
+                LastName = "Rock",
+                Address = "1000 Cookin Lane",
+                EmailAddress = "the.rock@gmail.com",
                 PhoneNumber = "none",
-                Position = "12 yr old",
-                Department = "Jr High",
-                StartDate = "July 2007",
+                Position = "The People's Elbow'",
+                Department = "The Gym",
+                StartDate = "Many years ago",
                 EndDate = "N/A",
                 EmploymentStatus = "active",
                 Shift = "24/7 for one whole year",
-                Manager = "Parents",
+                Manager = "He is his own",
                 Photo = "None",
                 FavoriteColor = "Clear"
             }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using TMM_Asp.Models;
 using TMM_Asp.Data;
 
@@ -15,50 +16,33 @@ namespace TMM_Asp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
 
-            using (var db = new DBModel())
-            {
-                // Create
-                Console.WriteLine("Inserted New Team Member");
-                db.Add(new MemberModel {
-                    FirstName = "Dave",
-                    LastName = "Manoa",
-                    Address = "554 E 1150 N",
-                    EmailAddress = "dave.manoa@gmail.com",
-                    PhoneNumber = "801-222-3333",
-                    Position = "Bossman",
-                    Department = "The World",
-                    StartDate = "Long time ago",
-                    EndDate = "N/A",
-                    EmploymentStatus = "active",
-                    Shift = "24/7",
-                    Manager = "God",
-                    Photo = "None",
-                    FavoriteColor = "Green"
-                });
-                db.SaveChanges();
+            CreateDbIfNotExists(host);
 
-                // Read
-                Console.WriteLine("Read All Team Members");
-                var blog = db.Members
-                    .OrderBy(b => b.LastName)
-                    .First();
-
-                // Update
-                //Console.WriteLine("Updating the blog and adding a post");
-                //blog.EmailAddress = "https://devblogs.microsoft.com/dotnet";
-                //blog.Add(
-                //   );
-                //db.SaveChanges();
-
-                // Delete
-                Console.WriteLine("Deleted Team Member");
-                db.Remove(blog);
-                db.SaveChanges();
-            }
-
+            host.Run();
         }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<DBModel>();
+                    //context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
